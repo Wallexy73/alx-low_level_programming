2e29124a4859a59dac0847b6a12a1cfa93f1b59d
Wallexy73 <olawaleismail23@yahoo.com>
2022-09-22 07:47:36 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	
+
 	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
@@ -21,11 +21,11 @@ char *cap_string(char *s)
 		for (j = 0; j < 13; j++)
 		{
 			if (s[i] == spe[j])
-			{	
+			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
 					s[i + 1] -= 32;
-				}	
+				}
 			}
 		}
 	}
